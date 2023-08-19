bed0efddb36c95ac444fc4ab145744131ed33e69
Yury Halushko <snaipp1@yandex.by>
2023-08-19 17:21:12 +0300

    feat:add Hero sction

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,26 @@
-const Button = () => {
+const Button = ({ label,
+                    iconURL,
+                    backgroundColor,
+                    textColor,
+                    borderColor,
+                    fullWidth
+}) => {
     return (
-        <></>
+        <button
+            className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+            backgroundColor
+                ? `${backgroundColor} ${textColor} ${borderColor}`
+                : "bg-coral-red text-white border-coral-red"
+        } rounded-full ${fullWidth && "w-full"}`}
+        >
+            {label}
+            <img
+                src={iconURL}
+                alt={label}
+                className="ml-2 rounded-full w-5 h-5"
+            />
+        </button>
     );
 };
 
