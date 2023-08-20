4be7ed27a4050097253793bce87515d82e68caa4
Yury Halushko <snaipp1@yandex.by>
2023-08-20 14:26:27 +0300

    feat: add Services and Special Offers sections

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,17 +9,20 @@ const Button = ({ label,
         <button
             className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
       ${
-            backgroundColor
-                ? `${backgroundColor} ${textColor} ${borderColor}`
-                : "bg-coral-red text-white border-coral-red"
-        } rounded-full ${fullWidth && "w-full"}`}
+                backgroundColor
+                    ? `${backgroundColor} ${textColor} ${borderColor}`
+                    : "bg-coral-red text-white border-coral-red"
+            } rounded-full ${fullWidth && "w-full"}`}
         >
             {label}
-            <img
-                src={iconURL}
-                alt={label}
-                className="ml-2 rounded-full w-5 h-5"
-            />
+
+            {iconURL && (
+                <img
+                    src={iconURL}
+                    alt='arrow right icon'
+                    className='ml-2 rounded-full bg-white w-5 h-5'
+                />
+            )}
         </button>
     );
 };
